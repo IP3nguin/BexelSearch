19e8a8ce154d6051b64c78dd4ee1d89a8bcbb296
Conor Rourke <Conor Rourke>
2016-11-09 23:39:03 +0000

    trying to fix issues with moving the page over from the login page, might have to make a button thing to do it. but locked from making classes in firebase tonight.

--- splashJS.js
+++ splashJS.js
@@ -1,10 +1,13 @@
-function signUp() { //this function handles the sign up with the auth server as well as error handling and detail insertion
-    var email = document.getElementById('suemail').value;
-    var password = document.getElementById('supassword').value;
-    var userName = document.getElementById('suusername').value;
-    var firstName = document.getElementById('sufirstname').value;
-    var lastName = document.getElementById('sulastname').value; //declare and pull data from html form
+var newAcc = 0;
+var email, password, userName, firstName, lastName;
 
+function signUp() { //this function handles the sign up with the auth server as well as error handling and detail insertion
+    email = document.getElementById('suemail').value;
+    password = document.getElementById('supassword').value;
+    userName = document.getElementById('suusername').value;
+    firstName = document.getElementById('sufirstname').value;
+    lastName = document.getElementById('sulastname').value; //declare and pull data from html form
+    newAcc = 1;
 
     if (email.length < 4) {
         alert('Please enter an email address.');
@@ -21,10 +24,10 @@ function signUp() { //this function handles the sign up with the auth server as 
 
         alert(errorMessage);
         console.log(error);
-
     });
-    
-    saveDetails(userName, firstName, lastName, email); // call the function to insert user details to database, currently has timing errors
+
+    //saveDetails(userName, firstName, lastName, email); // call the function to insert user details to database, currently has timing errors
+
 }
 
 function logIn() { //this function handles login and error handling
@@ -42,17 +45,13 @@ function logIn() { //this function handles login and error handling
         } else {
             alert(errorMessage);
         }
-        console.log(error); //log the errors for debugging dont know how to view though :P
+        console.log(error); //log the errors for debugging
 
     });
-
-
-
 }
 
-function saveDetails(user, first, last, email) { //takes the user detaisl as input then puts them into the "users" object in the database
+function saveDetails(user, first, last, email, uId) { //takes the user detaisl as input then puts them into the "users" object in the database
     var database = firebase.database();
-    var uId = firebase.auth().currentUser.uid;
 
     firebase.database().ref('users/' + uId).set({ // creats new objects in the database with the name = their userid
         "username": user,
@@ -61,17 +60,31 @@ function saveDetails(user, first, last, email) { //takes the user detaisl as inp
         "email": email
     });
 
+    document.write('<button type="button" id="SignUpBut" onclick="redirect()">Sign up sucessful!</button>');
+
+
 }
 
-window.onload = function () { // load the function that moves you on when you are signed in
-    //   watchLogin(); //currently breaking user data saving
-}
+
+
 
 function watchLogin() {
 
-    firebase.auth().onAuthStateChanged(function (user) { // listen for login status 
-        if (user) { //if logged in, move user to the homepage
-            window.location = 'homePage.html'; //please change this to location of our home page
+    firebase.auth().onAuthStateChanged(function (user) {
+        if ((user) && (newAcc == 1)) {
+            var uId = firebase.auth().currentUser.uid;
+            saveDetails(userName, firstName, lastName, email, uId);
+
+        } else if (user) {
+            window.location = 'homePage.html';
         }
     });
+}
+
+window.onload = function () {
+    watchLogin();
+}
+
+function redirect() {
+    window.location = 'homePage.html';
 }
