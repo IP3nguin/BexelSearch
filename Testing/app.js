6fff344c4b0edc51eaa1c62207b1a431fb285cdb
Benjamin Korady <benjamin.korady.2014@mumail.ie>
2016-11-11 19:11:13 +0000

    Added some comments to explain what's actually going on

--- Testing/app.js
+++ Testing/app.js
@@ -1,11 +1,11 @@
 function print() {
 	
 	const preObject = document.getElementById('JSONout');
-	const dbRefClasses = firebase.database().ref().child('classes');
+	const dbRefClasses = firebase.database().ref().child('classes');	//Creates a reference to the actual database object 'classes'
 	
-	dbRefClasses.on('value', snap => {
+	dbRefClasses.on('value', snap => {	//This reads from the database in real time and updates every time a 'value' changes in the object
 	console.log(snap.val());
-	preObject.innerText = JSON.stringify(snap.val(), null, 3);
+	preObject.innerText = JSON.stringify(snap.val(), null, 3);	//Change the <pre> to a JSONified classes object
 });
 }
 
