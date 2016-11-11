f95147d86c7c20828369ba4215070d49cc018bb0
Benjamin Korady <benjamin.korady.2014@mumail.ie>
2016-11-11 19:07:39 +0000

    Was fiddling around with firebase realtime database updating, ajax, xml, php and debugging for the past 9 hours, figured out a realtime database pull. Now to figure out how to push into the database

---
+++ Testing/connectToFirebase.js
@@ -0,0 +1,8 @@
+var config = {
+		apiKey: " AIzaSyCHb_Ra7dl02OImTNyEnLoGek4IA5Ck84g ",
+		authDomain: "bexel-ed8b3.firebaseapp.com",
+		databaseURL: "https://bexel-ed8b3.firebaseio.com/",
+		storageBucket: "bexel-ed8b3.appspot.com"
+	  };
+firebase.initializeApp(config);
+
