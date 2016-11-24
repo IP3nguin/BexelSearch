c963ad73c00ae0aa6679b6a6ec77722829bdbd29
Conor Rourke <penguinrourke@gmail.com>
2016-11-24 22:38:19 +0000

    Final Formatting of files for deployment. TODO: Repoint references to files in all html.

--- public_html/Js/splashJS.js
+++ public_html/Js/splashJS.js
@@ -5,6 +5,13 @@ function signUp() { //this function handles the sign up with the auth server as 
     var firstName = document.getElementById('sufirstname').value;
     var lastName = document.getElementById('sulastname').value; //declare and pull data from html form
 
+function signUp() { //this function handles the sign up with the auth server as well as error handling and detail insertion
+    email = document.getElementById('suemail').value;
+    password = document.getElementById('supassword').value;
+    userName = document.getElementById('suusername').value;
+    firstName = document.getElementById('sufirstname').value;
+    lastName = document.getElementById('sulastname').value; //declare and pull data from html form
+    newAcc = 1;
 
     if (email.length < 4) {
         alert('Please enter an email address.');
@@ -14,17 +21,20 @@ function signUp() { //this function handles the sign up with the auth server as 
         alert('Please enter a longer password.');
         return;
     }
-
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) { // actual firebase fucntion to create the user, along with firebase error handling
+	
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function(uId){	
+	var uId = firebase.auth().currentUser.uid;
+	saveDetails(userName, firstName, lastName, email, uId);
+	
+	
+}).catch(function (error) { // actual firebase fucntion to create the user, along with firebase error handling
         var errorCode = error.code;
         var errorMessage = error.message;
 
         alert(errorMessage);
-        console.log(error);
-
+        console.log(error);	
     });
     
-    saveDetails(userName, firstName, lastName, email); // call the function to insert user details to database, currently has timing errors
 }
 
 function logIn() { //this function handles login and error handling
@@ -32,8 +42,11 @@ function logIn() { //this function handles login and error handling
     var email = document.getElementById('logEmail').value;
     var password = document.getElementById('logPass').value; //declare and pull data from html form.
 
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) { //actual firebase function for sign in, handles errors as well
-
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
+	
+	window.location = "/home/u170158/CS353/Bexel/homePage.html";
+	
+	}).catch(function (error) { //actual firebase function for sign in, handles errors as well
         var errorCode = error.code;
         var errorMessage = error.message;
 
@@ -42,36 +55,38 @@ function logIn() { //this function handles login and error handling
         } else {
             alert(errorMessage);
         }
+
         console.log(error); //log the errors for debugging dont know how to view though :P
-
     });
-
-
-
 }
 
-function saveDetails(user, first, last, email) { //takes the user detaisl as input then puts them into the "users" object in the database
+function saveDetails(user, first, last, email, uId) { //takes the user detaisl as input then puts them into the "users" object in the database
     var database = firebase.database();
-    var uId = firebase.auth().currentUser.uid;
 
     firebase.database().ref('users/' + uId).set({ // creats new objects in the database with the name = their userid
         "username": user,
         "firstName": first,
         "lastName": last,
         "email": email
-    });
+    }).then(function(user){
+	
+	window.location = "/home/u170158/CS353/Bexel/homePage.html";
+	
+	});
+
+    document.write('<button type="button" id="SignUpBut" onclick="redirect()">Sign up sucessful!</button>');
+
 
 }
+$(document).ready(function() { // load the function that moves you on when you are signed in
+	var user;
+	var unsub = firebase.auth().onAuthStateChanged(function(user){
+		user = firebase.auth().currentUser;
+	 	if (user) { //if logged in, move user to the homepage
+		   	window.location = '/home/u170158/CS353/Bexel/homePage.html'; //please change 			this to location of our home page
+		}
+	unsub();
+	});
+	
 
-window.onload = function () { // load the function that moves you on when you are signed in
-    //   watchLogin(); //currently breaking user data saving
-}
-
-function watchLogin() {
-
-    firebase.auth().onAuthStateChanged(function (user) { // listen for login status 
-        if (user) { //if logged in, move user to the homepage
-            window.location = 'homePage.html'; //please change this to location of our home page
-        }
-    });
-}
+});
