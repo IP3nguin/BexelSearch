c2fc2e4c0c4d9d3bb0bba37d9d4c89d11961811d
Ciara Walsh <ciarawalsh2109@gmail.com>
2016-11-29 22:14:37 +0000

    Changed redirect to point to home instead of Home

--- public_html/Js/splashJS.js
+++ public_html/Js/splashJS.js
@@ -83,7 +83,7 @@ $(document).ready(function () { // load the function that moves you on if you al
     var unsub = firebase.auth().onAuthStateChanged(function (user) { // create a listener to waits for firebase initialisation before attempting to get currentUser. 
         user = firebase.auth().currentUser;
         if (user) { //if logged in, move user to the homepage
-            window.location = 'Home';
+            window.location = 'home';
         }
         unsub(); //unsub() here is used to listen on pageload for a user if one is found then move on, if not then stop listening as the return value of unsub() is the function to kill the listener.
         //This is needed as the database connection becomes erratic for other function while listener is active.
