af53e2f607690eee1a5a1145716f8a02d89045e5
Ciara Walsh <ciarawalsh2109@gmail.com>
2016-12-06 14:36:48 +0000

    Various styling changes

--- public_html/Js/splashJS.js
+++ public_html/Js/splashJS.js
@@ -99,7 +99,7 @@ $(function () { // jQuery function to dynamically display the login and register
     $('#login-form-link').click(function (e) {
         $("#login-form").delay(100).fadeIn(100);
         $("#register-form").fadeOut(100);
-        $('#register-form-link').removeClass('active');
+        $('#register-form-link').removeClass('active2');
         $(this).addClass('active');
         e.preventDefault();
     });
@@ -107,7 +107,7 @@ $(function () { // jQuery function to dynamically display the login and register
         $("#register-form").delay(100).fadeIn(100);
         $("#login-form").fadeOut(100);
         $('#login-form-link').removeClass('active');
-        $(this).addClass('active');
+        $(this).addClass('active2');
         e.preventDefault();
     });
 
